91d6373bb893bd35b4ee2fbb2c7a9af23caa266a
udonna-cell <orabuezestanley@gmail.com>
2025-11-28 11:54:41 +0100

    added top quote route

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const randomQuote = require("./route/random");
 const qouteCategory = require("./route/category");
 const authorQuote = require('./route/author');
+const topQuote = require("./route/top.js");
 
 const app = express();
 const port = 3000;
@@ -11,7 +12,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-
+app.use("/quotes/top", topQuote);
 app.use("/quotes/random", randomQuote);
 app.use("/quotes/category", qouteCategory);
 app.use("/quotes/author", authorQuote);
